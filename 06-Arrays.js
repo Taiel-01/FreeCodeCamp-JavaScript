b24f700a6a960b2f667d610c57a90e0974b9201b
Taiel Nunes <taielalen56@hotmail.com>
2021-05-25 23:35:01 -0300

    Agrego recopilación sobre Arrays

---
+++ 06-Arrays.js
@@ -0,0 +1,28 @@
+/* =======================
+?        Arrays
+======================= */
+
+//* Crear arreglo
+var myArray = ["manzanas", 20];
+
+//* Arreglo anidado
+var myArray = [["manzanas", 20], ["peras", 25]];
+
+//* Acceder a propiedades del arreglo
+var myArray = [50, 60, 70];
+var myData = myArray[0]
+
+//* Modificar datos de un arreglo
+var myArray = [18, 64, 99];
+myArray[0] = 45;
+
+//* Acceder a arreglos multidimensionales con índices 
+var myArray = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [[10, 11, 12], 13, 14]
+];
+var myData = myArray[3][0][1] // 11
+
+
---
+++ 07-Array_Methods.js
@@ -0,0 +1,22 @@
+/* =======================
+?    Arrays Methods
+======================= */
+
+//* .push() - Empuja al final del arreglo
+var myArray = [["John", 23], ["cat", 2]];
+myArray.push(["dog", 3])
+
+
+//* .pop() - Elimina el ultimo elemento
+var myArray = [["John", 23], ["cat", 2]];
+myArray.pop();
+
+
+//* .shift() - Elimina el primer elemento 
+var myArray = [["John", 23], ["cat", 2]];
+myArray.shift();
+
+
+//* .unshift() - Agrega el primer elemento 
+var myArray = [["John", 23], ["cat", 2]];
+myArray.unshift(["Paul",35]);
