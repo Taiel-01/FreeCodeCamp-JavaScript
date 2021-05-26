9065e3bfab30f2b6a5ae7353585a31645580646b
Taiel Nunes <taielalen56@hotmail.com>
2021-05-25 23:33:38 -0300

    Agrego recopilación sobre Asignación Compuesta

---
+++ 03-Asignacion_Compuesta.js
@@ -0,0 +1,42 @@
+/* =======================
+?  Asignación compuesta
+======================= */
+
+//* Asignación compuesta con adición aumentada
+var a = 3;
+var b = 17;
+var c = 12;
+
+a += 12;
+b += 9;
+c += 7;
+
+
+//* Asignación compuesta con resta aumentada
+var a = 11;
+var b = 9;
+var c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
+
+
+//* Asignación compuesta con multiplicación aumentada
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+a *= 5;
+b *= 3;
+c *= 10;
+
+
+//*  Asignación compuesta con división aumentada
+var a = 48;
+var b = 108;
+var c = 33;
+
+a /= 12;
+b /= 4;
+c /= 11;
