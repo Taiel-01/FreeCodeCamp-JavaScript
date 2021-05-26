e918b1b5f2edc97cff10a0e5f7ed575ba15816d4
Taiel Nunes <taielalen56@hotmail.com>
2021-05-25 23:23:04 -0300

    Agrego recopilación sobre Variables

---
+++ 01-Variables.js
@@ -0,0 +1,23 @@
+/*
+* Tipos de variables
+? undefined, 
+? null, 
+? boolean, 
+? string, 
+? symbol, 
+? bigint, 
+? number, 
+? object.
+*/
+
+//* Incializar una variable
+var myName;
+myName = 5;
+
+//* Darle el valor de una variable a otra
+var ourName;
+ourName = myName;
+
+//* Escribir en camelCase como buena practica
+var camelCase;
+camelCase = "A String";
