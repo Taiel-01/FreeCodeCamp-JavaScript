d2d2f2705bb4b751a0058f81259f05f3bdfb1f16
Taiel Nunes <taielalen56@hotmail.com>
2021-05-25 23:34:44 -0300

    Agrego recopilación sobre Notacion de Corchetes

---
+++ 05-Notacion_De_Corchetes.js
@@ -0,0 +1,21 @@
+/* =======================
+?  notacion de corchetes
+======================= */
+    
+//*  Extraer valor de una cadena
+//? 1# encontrar primer valor
+    var firstLetterOfMyName = "";
+    var myName = "Taiel";
+    firstLetterOfMyName = myName[0]; // T
+
+//? 2# encontrar valorintermedio
+    var myName = "Taiel";
+    var thirdLetterOfmyName = myName[2]; // i
+
+//? 3# encontrar ultimo valor
+    var myName = "Taiel";
+    var lastLetterOfmyName = myName[myName.length -1]; 
+
+//? 4# encontrar ante ultimo valor
+    var myName = "Taiel";
+    var SecondlastLetterOfmyName = myName[myName.length -2]; 
