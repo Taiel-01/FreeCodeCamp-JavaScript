b8921b8403ed6f54c79f23a2fcd5c85f1bc6d4c1
Taiel Nunes <taielalen56@hotmail.com>
2021-05-25 23:33:03 -0300

    Agrego recopilación sobre Operadores Aritmeticos

---
+++ 02-Operadores-Aritmeticos.js
@@ -0,0 +1,33 @@
+/* =======================
+? Operadores Aritmeticos
+======================= */
+
+
+//* numero entero
+var myInt = 2;
+
+//* numero decimal
+var myDecimal = 2.5;
+
+//* operador de suma
+var sum = 2 + 2;
+
+//* operador de resta
+var difference = 2 - 2;
+
+//* operador de multiplicacion
+var product = 2 * 2;
+
+//* operador de divicion
+var quotient = 2 / 2;
+
+//* operador de resto
+var remainder = 10 % 3;
+
+//* incremento
+var myVar = 2;
+myVar++;
+
+//* decremento
+var myVar = 2;
+myVar--;
